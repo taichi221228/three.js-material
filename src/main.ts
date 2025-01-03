bab01d750ce79542b8b4e3585f3b26b78d6a6321
たいち ひ <taichi221228@icloud.com>
2025-01-03 13:28:20 +0900

    feat: Add canvas element and basic rendering setup
    A `<canvas>` was added to the HTML for rendering purposes.

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
 import "./style.css";
+import { setup } from "./setup.ts";
+
+const { camera, renderer, scene } = setup();
+
+(function render() {
+	requestAnimationFrame(render);
+	renderer.render(scene, camera);
+})();
