26e6556a67e7a460feac5a06656bd858a7623183
たいち ひ <taichi221228@icloud.com>
2025-01-03 15:01:05 +0900

    feat: Add rotating torus to the scene
    Introduced a TorusGeometry with a basic material and added it to the scene.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,27 @@
 import "./style.css";
+
+import { Mesh, MeshBasicMaterial, TorusGeometry } from "three";
 import { setup } from "./setup.ts";
 
 const { camera, renderer, scene } = setup();
 
+const geometry = new TorusGeometry(40, 15);
+const materials = [new MeshBasicMaterial({ color: 0x6699ff })];
+const meshes = [];
+
+for (const material of materials) {
+	const mesh = new Mesh(geometry, material);
+	meshes.push(mesh);
+	scene.add(mesh);
+}
+
 (function render() {
 	requestAnimationFrame(render);
+
+	for (const mesh of meshes) {
+		mesh.rotation.x += 0.025;
+		mesh.rotation.y += 0.025;
+	}
+
 	renderer.render(scene, camera);
 })();
